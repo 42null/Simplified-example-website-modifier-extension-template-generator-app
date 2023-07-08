f3c1c9af158f7a01c6eb382c09209e826f6ef6b9
42null <averyparanoiduser@gmail.com>
2023-07-08 00:30:12 -0500

    Movement to JsonController.cs, layout tree movement, bug with generating json object from layout tree

---
+++ ExtensionGenerator/JsonController.cs
@@ -0,0 +1,231 @@
+namespace ExtensionGenerator;
+
+using System;
+using System.Text.Json;
+using Microsoft.Maui;
+using static System.Net.Mime.MediaTypeNames;
+using Microsoft.Maui.Controls;
+using Microsoft.Maui.Controls.Xaml;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+using Microsoft.Maui.Storage;
+using Microsoft.Maui.Graphics;
+using Microsoft.Maui.Accessibility;
+using System.Diagnostics;
+using System.Text.Json.Nodes;
+using Microsoft.Maui.HotReload;
+
+
+public class JsonController
+{
+    //TODO: MOVE!!!
+    private readonly int ICON_WIDTH = 15;
+    private readonly static int SETTINGS_WIDTH = 400;
+    private readonly static int SETTINGS_WIDTH_LABEL = SETTINGS_WIDTH / 2; // Set here so math is not repeated
+    private readonly static int SETTINGS_WIDTH_ENTRY = SETTINGS_WIDTH / 2; // Set here so math is not repeated
+    private readonly Thickness SETTINGS_SPACEING = new (0, 10, 10, 0);
+    private readonly int SECTOR_HEADER_FONT_SIZE = 22;
+    
+    
+    private String jsonFilename;
+    private ColorInterpreter colorController;
+    
+    //Stored generateable
+    private Layout stackLayout;
+    private JsonObject JsonObject;
+    private Array storedHashes; //TODO: Upgrade to hashmap equivalent
+    
+    public JsonController(String filename, ColorInterpreter colorController)
+    {
+        this.jsonFilename = filename;
+        this.colorController = colorController;
+    }
+
+    public async Task<Layout> GetStackFromFile()
+    {
+        // if (stackLayout == null)
+        // {
+        stackLayout = await LoadMauiAssetJSONAsync(jsonFilename);
+        // }
+        return stackLayout;
+    }
+
+    public async Task<JsonObject> GenerateJsonFromSettings()
+    {
+        JsonObject primaryObject = new JsonObject();
+
+        // stackLayout = await GetStackFromFile();
+        Console.WriteLine(stackLayout.Children.ToString());
+        Console.WriteLine("----------");
+        Console.WriteLine(stackLayout.Children[1]);
+        
+        // Action<Layout> traverse = null;
+        Func<Layout, JsonNode?> traverse = null;
+        traverse = (n) =>
+        {
+            JsonObject jsonObject = new JsonObject();
+            
+            foreach (Element element in n.Children)
+            {
+                // Check if the child is a layout
+                if (element is Layout childLayout)
+                {
+                    // jsonObject.Add(  traverse(childLayout));
+                    // jsonObject.Add(Guid.NewGuid().ToString(), traverse(childLayout));
+
+                    if (String.IsNullOrEmpty(childLayout.AutomationId))
+                    {
+                        JsonNode? node = traverse(childLayout);
+                        // node.GetValue();
+                        // if (node is JsonObject jsonObjectFromNode)
+                        // {
+                        //     jsonObjectFromNode.GetValue<>()
+                        // }
+                    }
+                    else
+                    {
+                        jsonObject.Add(childLayout.AutomationId, traverse(childLayout));
+                    }
+
+                }else if (element is Entry entry)
+                {
+                    // Console.WriteLine("Entry Text = "+entry.Text);
+                    if (!string.IsNullOrEmpty(entry.Text)){
+                        jsonObject.Add(entry.AutomationId, entry.Text);
+                    }
+                }
+            }
+
+            return jsonObject;
+        };
+        JsonNode? result = traverse(stackLayout);
+        Console.WriteLine("result = "+result);
+        
+        return primaryObject;
+    }
+
+    
+    private async Task<Layout> LoadMauiAssetJSONAsync(String fileName)
+    {
+        try
+        {
+            await using var stream = await FileSystem.OpenAppPackageFileAsync(fileName);
+            using var reader = new StreamReader(stream);
+            
+
+            var jsonAsStringContents = reader.ReadToEnd();
+            // Console.WriteLine(jsonAsStringContents);
+            Sector sector = JsonSerializer.Deserialize<Sector>(jsonAsStringContents);
+
+            StackLayout sectorStack = await CreateSectorElementAsync(sector, colorController);
+
+            sectorStack.Margin = new Thickness(0, 20, 210, 20); //TODO: Make programmatic once icon size is working
+            
+            // topInfoArea.Children.Add(sectorStack);
+
+            // JsonObject generatedExportJson = GenerateJsonFromSettings();
+            
+            
+            return sectorStack;
+        }
+        catch (Exception ex)
+        {
+            // Console.WriteLine($"Error attempting to use settings from file \"{fileName}\" with error {ex.Message}.");
+            throw new ArgumentException($"Error attempting to use settings from file \"{fileName}\" with error {ex.Message}.");
+        }
+    }
+
+
+    async Task<StackLayout> CreateSectorElementAsync(Sector sector, ColorInterpreter colorController)
+    {
+        StackLayout sectorElement = new StackLayout
+        {
+            Orientation = StackOrientation.Vertical,
+            HorizontalOptions = LayoutOptions.Center,
+            // Margin = new Thickness(0,20,110,20) //TODO: Make programmatic once icon size is working
+            Margin = new Thickness(110,20,0,20), //TODO: Make programmatic once icon size is working
+            AutomationId = sector.SettingKey
+        };
+
+        Label header = new Label
+        {
+            Text = sector.Header,
+            VerticalOptions = LayoutOptions.Center,
+            HorizontalTextAlignment = TextAlignment.End,
+            WidthRequest = SETTINGS_WIDTH_LABEL,
+            FontSize = SECTOR_HEADER_FONT_SIZE
+        };
+
+        sectorElement.Children.Add(header);
+
+        if (sector.Settings is { Count: > 0 })
+        {
+            foreach (Setting setting in sector.Settings)
+            {
+                StackLayout settingElement = new StackLayout
+                {
+                    Orientation = StackOrientation.Horizontal,
+                    HorizontalOptions = LayoutOptions.Center,
+                };
+                
+                Label label = new Label
+                {
+                    Text = setting.Name + ": ",
+                    VerticalOptions = LayoutOptions.Center,
+                    HorizontalTextAlignment = TextAlignment.End,
+                    WidthRequest = SETTINGS_WIDTH_LABEL,
+                    Padding = SETTINGS_SPACEING
+                };
+                Entry entry = new Entry
+                {
+                    Text = setting.Value,
+                    Placeholder = setting.PlaceHolder,
+                    VerticalOptions = LayoutOptions.Center,
+                    WidthRequest = SETTINGS_WIDTH_ENTRY,
+                    BackgroundColor = colorController.GetPrimary(),
+                    Margin = SETTINGS_SPACEING,
+                    AutomationId = setting.SettingKey
+                };
+                
+                //TODO: Because same, build icon once and clone?
+                ImageButton infoIcon = new ImageButton
+                {
+                    Source = "info.png",
+                    HeightRequest = ICON_WIDTH,
+                    WidthRequest = ICON_WIDTH,
+                };
+
+                // ToolTipProperties.SetText(infoIcon, "Click to Save your data");
+
+                settingElement.Children.Add(label);
+                settingElement.Children.Add(entry);
+                settingElement.Children.Add(infoIcon);
+                
+
+                //Adds period ending if one does not already exist.
+                ToolTipProperties.SetText(infoIcon,
+                    setting.Description + (setting.Description.EndsWith(".") ? "" : "."));
+                
+                sectorElement.Children.Add(settingElement);
+            }
+        
+        }
+
+        // Recursive call to process child sectors
+        if (sector.ChildSectors is { Count: > 0 })
+        {
+            // Console.WriteLine("ChildSectors true with "+sector.ChildSectors.Count + " for '"+sector.Header+"'");
+            foreach (var childSector in sector.ChildSectors)
+            {
+                // Console.WriteLine("Child sector: " + childSector.Header);
+                StackLayout childSectorElement = await CreateSectorElementAsync(childSector, colorController);
+                
+                sectorElement.Children.Add(childSectorElement);
+            }
+        }
+        
+        return sectorElement;
+    }
+}
--- ExtensionGenerator/MainPage.xaml.cs
+++ ExtensionGenerator/MainPage.xaml.cs
@@ -12,14 +12,21 @@ using Microsoft.Maui.Storage;
 using Microsoft.Maui.Graphics;
 using Microsoft.Maui.Accessibility;
 using System.Diagnostics;
+using System.Runtime.CompilerServices;
+using System.Text.Json.Nodes;
 using Microsoft.Maui.HotReload;
 
 namespace ExtensionGenerator;
 
+public enum SpecialType
+{
+    Color,
+    Image
+}
 public class Sector
 {
     [JsonPropertyName("header")]
-    public String Header { get; set; } // Rename to Name, if desired
+    public String Header { get; set; } //TODO: Rename to Name?
 
     [JsonPropertyName("settingKey")]
     public String SettingKey { get; set; }
@@ -29,10 +36,10 @@ public class Sector
 
     [JsonPropertyName("childSectors")]
     public List<Sector> ChildSectors { get; set; }
-    
+
     public Sector()
     {
-        // Ensure lists are initialized
+        // Ensures lists are initialized
         Settings = new List<Setting>();
         ChildSectors = new List<Sector>();
     }
@@ -45,7 +52,11 @@ public class Setting
 
     [JsonPropertyName("value")]
     public String Value { get; set; }
-
+    
+    [JsonPropertyName("specialType")]
+    [JsonConverter(typeof(JsonStringEnumConverter))]
+    public SpecialType SpecialType { get; set; }
+    
     [JsonPropertyName("description")]
     public String Description { get; set; }
 
@@ -54,7 +65,21 @@ public class Setting
 
     [JsonPropertyName("settingKey")]
     public String SettingKey { get; set; }
+
+    public Setting()
+    {
+        if (SpecialType == SpecialType.Color)
+        {
+            Description = "Use a 3 or 6 digit hexcode color value";
+            PlaceHolder = "#ffffff";
+        }
+        else
+        {
+            SpecialType = SpecialType.Image;
+        }
+    }
 }
+
 
 //TODO: Add revert to default option
 public partial class MainPage : ContentPage
@@ -63,12 +88,13 @@ public partial class MainPage : ContentPage
     private readonly int SETTINGS_WIDTH = 400; //TODO: Set off of screen
     private readonly int SETTINGS_WIDTH_LABEL;
     private readonly int SETTINGS_WIDTH_ENTRY;
-    private readonly Thickness SETTINGS_SPACEING = new Thickness(0, 10, 10, 0);
+    private readonly Thickness SETTINGS_SPACEING = new (0, 10, 10, 0);
     private readonly int SECTOR_HEADER_FONT_SIZE = 22;
     
     // Color Themes
-    private ColorInterpreter colorController = new ColorInterpreter("Primary", "Secondary", "Tertiary");
-    
+    private ColorInterpreter colorController = new ("Primary", "Secondary", "Tertiary");
+    private JsonController jsonController;
+
     int count = 0;
     VerticalStackLayout mainLayout;
     Layout topInfoArea;
@@ -82,126 +108,31 @@ public partial class MainPage : ContentPage
         
         // Set theme colors
         
-
         //Get the sections that already exist in MainPage.xaml.
         topInfoArea = (VerticalStackLayout)FindByName("top_info_area");
         
-
         //Genenerate label-entry pairs from file
-        LoadMauiAssetJSONAsync("defaults.json");
+        // jsonController = new JsonController("defaults.json", colorController);
 
-    }
-
-    async Task LoadMauiAssetJSONAsync(String fileName)
-    {
-        try
-        {
-            await using var stream = await FileSystem.OpenAppPackageFileAsync(fileName);
-            using var reader = new StreamReader(stream);
-            
-
-            var jsonAsStringContents = reader.ReadToEnd();
-            // Console.WriteLine(jsonAsStringContents);
-            Sector sector = JsonSerializer.Deserialize<Sector>(jsonAsStringContents);
-
-            StackLayout sectorElement = await CreateSectorElementAsync(sector);
-
-            sectorElement.Margin = new Thickness(0, 20, 210, 20); //TODO: Make programmatic once icon size is working
-            
-            topInfoArea.Children.Add(sectorElement);
-        }
-        catch (Exception ex)
-        {
-            Console.WriteLine($"Error attempting to use settings from file \"{fileName}\" with error {ex.Message}.");
-            return;
-        }
-    }
-
-    async Task<StackLayout> CreateSectorElementAsync(Sector sector)
-    {
-        StackLayout sectorElement = new StackLayout
-        {
-            Orientation = StackOrientation.Vertical,
-            HorizontalOptions = LayoutOptions.Center,
-            // Margin = new Thickness(0,20,110,20) //TODO: Make programmatic once icon size is working
-            Margin = new Thickness(110,20,0,20) //TODO: Make programmatic once icon size is working
-        };
-
-        Label header = new Label
-        {
-            Text = sector.Header,
-            VerticalOptions = LayoutOptions.Center,
-            HorizontalTextAlignment = TextAlignment.End,
-            WidthRequest = SETTINGS_WIDTH_LABEL,
-            FontSize = SECTOR_HEADER_FONT_SIZE
-        };
-
-        sectorElement.Children.Add(header);
-
-        if (sector.Settings is { Count: > 0 })
-        {
-            foreach (Setting setting in sector.Settings)
-            {
-                StackLayout settingElement = new StackLayout
-                {
-                    Orientation = StackOrientation.Horizontal,
-                    HorizontalOptions = LayoutOptions.Center,
-                };
-                
-                Label label = new Label
-                {
-                    Text = setting.Name + ": ",
-                    VerticalOptions = LayoutOptions.Center,
-                    HorizontalTextAlignment = TextAlignment.End,
-                    WidthRequest = SETTINGS_WIDTH_LABEL,
-                    Padding = SETTINGS_SPACEING
-                };
-                Entry entry = new Entry
-                {
-                    Text = setting.Value,
-                    Placeholder = setting.PlaceHolder,
-                    VerticalOptions = LayoutOptions.Center,
-                    WidthRequest = SETTINGS_WIDTH_ENTRY,
-                    BackgroundColor = colorController.GetPrimary(),
-                    Margin = SETTINGS_SPACEING
-                };
-                //TODO: Because same, build icon once and clone?
-                ImageButton infoIcon = new ImageButton
-                {
-                    Source = "info.png",
-                    HeightRequest = ICON_WIDTH,
-                    WidthRequest = ICON_WIDTH,
-                };
-
-                // ToolTipProperties.SetText(infoIcon, "Click to Save your data");
-
-                settingElement.Children.Add(label);
-                settingElement.Children.Add(entry);
-                settingElement.Children.Add(infoIcon);
-
-                //Adds period ending if one does not already exist.
-                ToolTipProperties.SetText(infoIcon,
-                    setting.Description + (setting.Description.EndsWith(".") ? "" : "."));
-                
-                sectorElement.Children.Add(settingElement);
-            }
+        // Task<Layout> jsonLayout = jsonController.GetStackFromFile();
+        // topInfoArea.Children.Add(await jsonLayout);
         
-        }
+        // LoadMauiAssetJSONAsync("defaults.json");
 
-        // Recursive call to process child sectors
-        if (sector.ChildSectors is { Count: > 0 })
-        {
-            Console.WriteLine("ChildSectors true with "+sector.ChildSectors.Count + " for '"+sector.Header+"'");
-            foreach (var childSector in sector.ChildSectors)
-            {
-                // Console.WriteLine("Child sector: " + childSector.Header);
-                StackLayout childSectorElement = await CreateSectorElementAsync(childSector);
-                sectorElement.Children.Add(childSectorElement);
-            }
-        }
-        
-        return sectorElement;
+        InitializeAsync();
     }
+
+    private async void InitializeAsync()
+    {
+        jsonController = new JsonController("defaults.json", colorController);
+        Layout jsonLayout = await jsonController.GetStackFromFile();
+        topInfoArea.Children.Add(jsonLayout);
+        
+        
+        jsonController.GenerateJsonFromSettings();
+    }
+
+
 
     // private void OnCounterClicked(object sender, EventArgs e)
     // {
