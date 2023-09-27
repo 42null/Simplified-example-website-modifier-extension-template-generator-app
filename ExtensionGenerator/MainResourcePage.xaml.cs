9e8254dfdc64e98dae05afff36bce49d13d396d8
42null <averyparanoiduser@gmail.com>
2023-09-27 08:06:00 -0500

    Improved image prefrences helper page list visuals

--- ExtensionGenerator/MainResourcePage.xaml.cs
+++ ExtensionGenerator/MainResourcePage.xaml.cs
@@ -10,9 +10,10 @@ using System.IO;
 // TODO: Getting drag and drop into to work on different os's is taking longer than expected. Right now a file picker is being used instead  
 public partial class MainResourcePage : ContentPage
 {
-    private readonly int ICON_WIDTH = 15;
-    private readonly int RESOURCE_FILENAME_FONT_SIZE = 32;
-    private readonly int RESOURCE_FULL_PATH_FONT_SIZE = 24;
+    private static readonly int ICON_WIDTH = 15;
+    private static readonly int RESOURCE_ELEMENT_DISPLAY_HEIGHT = 72;
+    private static readonly int RESOURCE_FILENAME_FONT_SIZE  = (int)(RESOURCE_ELEMENT_DISPLAY_HEIGHT * 0.4);
+    private static readonly int RESOURCE_FULL_PATH_FONT_SIZE = (int)(RESOURCE_ELEMENT_DISPLAY_HEIGHT * 0.2);
     
     Layout topInfoArea;
     Layout savedResourcesDisplayArea;
@@ -20,7 +21,7 @@ public partial class MainResourcePage : ContentPage
 
     private DropGestureRecognizer DropGestureRecognizer { get; set; }
     
-    private List<string> imageFilePaths = new List<string>();
+    private List<string> imageFilePaths;
     
     
     // Color Themes
@@ -42,20 +43,35 @@ public partial class MainResourcePage : ContentPage
             {
                 Console.WriteLine(imageFilePath);
                 
-
                 StackLayout resourceDisplay = new StackLayout
                 {
                     Orientation = StackOrientation.Horizontal,
                     VerticalOptions = LayoutOptions.Center,
+                    HorizontalOptions = LayoutOptions.Center,
                     Background = Colors.Aqua,//TODO: MAKE USE PROGRAMMATIC VALUES!
+                    Margin = new Thickness(5,5,5,5),
+
+                    // Margin = new Thickness(20,20,20,20), //TODO: Make programmatic once icon size is working
+                    HeightRequest = RESOURCE_ELEMENT_DISPLAY_HEIGHT,
+                    WidthRequest = (int)(DeviceDisplay.MainDisplayInfo.Width * 0.35),
                     
-                    // Margin = new Thickness(0,20,110,20) //TODO: Make programmatic once icon size is working
-                    Margin = new Thickness(110,20,0,20), //TODO: Make programmatic once icon size is working
                     AutomationId = imageFilePath.GetHashCode().ToString()
                 };
+                StackLayout verticalResourceElementsSegment = new StackLayout
+                {
+                    Orientation = StackOrientation.Vertical,
+                    HorizontalOptions = LayoutOptions.FillAndExpand,
+                    WidthRequest = (int)(resourceDisplay.WidthRequest * 0.80),
+                    // AutomationId = 
+                };
+                
                 Image resourceImage = new Image
                 {
-                    Source = imageFilePath
+                    Source = imageFilePath,
+                    HorizontalOptions = LayoutOptions.Start,
+                    Margin = new Thickness(5,5,5,5),
+                    HeightRequest = RESOURCE_ELEMENT_DISPLAY_HEIGHT,
+                    WidthRequest = (int)(resourceDisplay.WidthRequest - verticalResourceElementsSegment.WidthRequest),
                 };
                 //TODO: !!!!!!!!!!! Make multi-platform character switch. HIGH PRIORITY
                 string fileName = imageFilePath.Substring(imageFilePath.LastIndexOf("/")+1);
@@ -64,6 +80,7 @@ public partial class MainResourcePage : ContentPage
                     Text = fileName,
                     TextColor = Colors.Black,
                     VerticalOptions = LayoutOptions.Start,
+                    HorizontalOptions = LayoutOptions.Start,
                     FontSize = RESOURCE_FILENAME_FONT_SIZE,
 
                     HorizontalTextAlignment = TextAlignment.Center,
@@ -75,21 +92,30 @@ public partial class MainResourcePage : ContentPage
                     Text = imageFilePath,
                     TextColor = Colors.Gray,
                     VerticalOptions = LayoutOptions.Start,
+                    HorizontalOptions = LayoutOptions.Start,
                     FontSize = RESOURCE_FULL_PATH_FONT_SIZE,
-
+                    // BackgroundColor = Colors.Beige,
                     HorizontalTextAlignment = TextAlignment.Center,
                     
                     // WidthRequest = SETTINGS_WIDTH_LABEL,
                 };
                 resourceDisplay.Children.Add(resourceImage);
-                resourceDisplay.Children.Add(fileNameHeader);
-                resourceDisplay.Children.Add(fileNameFullPath);
                 
+                verticalResourceElementsSegment.Children.Add(fileNameHeader);
+                verticalResourceElementsSegment.Children.Add(fileNameFullPath);
                 
-                display_saved_resources_area.Children.Add(resourceDisplay);
+                resourceDisplay.Children.Add(verticalResourceElementsSegment);
 
-
-                // savedResourcesDisplayArea.Children.Add(resourceDisplay);
+                Frame resourceDisplayFrame = new Frame // Add to frame so a corner radius can be achieved
+                {
+                    IsClippedToBounds = true, 
+                    CornerRadius = 10,
+                    Padding = new Thickness(2,-2,-2,-2),
+                    Content = resourceDisplay,
+                    WidthRequest = resourceDisplay.WidthRequest
+                };
+                
+                display_saved_resources_area.Children.Add(resourceDisplayFrame);
             }
         }
     }
